4601ac37dcd2068cf0e48aa56bd0519786545a04
koyexes <koyagabriel@gmail.com>
2017-10-16 04:50:21 +0100

    version api

---
+++ api/app/lib/api_version.rb
@@ -0,0 +1,23 @@
+# This class checks the version of api a request was made to
+# by checking the value of the :accept key in the headers hash
+# of the request object
+class ApiVersion
+  attr_reader :version, :default
+  def initialize(version, default = false)
+    @version = version
+    @default = default
+  end
+
+  # check whether version is specified or is default
+  def matches?(request)
+    check_headers request.headers || default
+  end
+
+  private
+
+  def check_headers(headers)
+    # check version from Accept headers; except custom media type `books`
+    accept = headers[:accept]
+    accept && accept.include?("application/vnd.books.#{version}+json")
+  end
+end
